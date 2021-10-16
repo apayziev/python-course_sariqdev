f98ac9900e66f65b63b13be684a6b1e5c45ab44e
wiut00012387 <84310155+apayziev@users.noreply.github.com>
2021-10-16 19:14:25 +0500

    Revert "Revert "Files added to repos""
    This reverts commit 26b56901b3834a763f3a429a1229c0c203468dd1.

---
+++ 2-dars.Amaliyot/hello-world.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 17 11:14:37 2021
+
+@author: CyberSecurityWarrior
+"""
+
+print("Hello world!")
+print('Asssalomu alekum')
---
+++ 5-dars.Amaliyot/5-dars.py
@@ -0,0 +1,39 @@
+#05-DARS. STRING (MATN)
+
+#Amaliyot
+#2)
+kocha="Ezgulik"
+mahalla="Xo\'ja"
+tuman="Parkent"
+viloyat="Toshkent"
+
+#3)
+# print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+#       tuman + " tumani, " + viloyat + " viloyati.")
+
+# #4)
+# print(" \n Iltimos, quyidagi ma'lumotlarni kiriting : ")
+# kocha = input( "Ko'changiz : ")
+# mahalla = input("Mahallangiz : ")
+# tuman = input("Tumaningiz : ")
+# viloyat = input("Viloyatingiz :")
+# print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+#       tuman + " tumani, " + viloyat + " viloyati")   
+
+#5)
+# print(kocha + " ko'chasi,\n" + mahalla + " mahallasi,\n" + \
+#       tuman + " tumani,\n" + viloyat + " viloyati")
+
+#6)
+manzil = (f"{kocha} ko\'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+print(manzil)
+print(manzil.title())
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.capitalize())
+
+# meva = "     olmani     "
+# print("Men " + meva.lstrip() + " yaxshi ko'raman")
+# print("Men " + meva.rstrip() + " yaxshi ko'raman")
+# print("Men " + meva.strip() + " yaxshi ko'raman")
+# print("Men " + meva + " yaxshi ko'raman")
