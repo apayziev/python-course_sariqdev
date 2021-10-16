9d9a951353108cd1de7a62e2660e6bb11c4941dc
wiut00012387 <84310155+apayziev@users.noreply.github.com>
2021-10-16 19:21:17 +0500

    6-lesson added

---
+++ 6-dars.Amaliyot/6-dars.py
@@ -0,0 +1,46 @@
+#06-DARS. SONLAR.
+
+#Amaliyot
+
+#1)
+
+# son = int(input("Sonni kiriting : "))
+# son_kvadrati = pow(son, 2)
+# son_kubi = pow(son, 3)
+# print(son , "ning kvadrati" , son_kvadrati, "ga teng")
+# print(son , "ning kubi" , son_kubi, "ga teng")
+
+#2)
+
+# yosh = int(input("Yoshingiz nechida? \n"))
+# sizning_yoshingiz = 2021 - yosh
+# print("Siz", sizning_yoshingiz, "da tug'ilgansiz")
+
+#3)
+
+#1-usul
+
+# birinchi_son = int(input("Birinchi sonni kiriting: "))
+# ikkinchi_son = int(input("Ikkinchi sonni kiriting: "))
+# print(birinchi_son,"+", ikkinchi_son,"=",birinchi_son+ikkinchi_son)
+# print(birinchi_son,"-", ikkinchi_son,"=",birinchi_son-ikkinchi_son)
+# print(birinchi_son,"*", ikkinchi_son,"=",birinchi_son*ikkinchi_son)
+# print(birinchi_son,"/", ikkinchi_son,"=",birinchi_son/ikkinchi_son)
+
+#2-usul
+
+# a = float(input("Birinchi sonni kiriting: "))
+# b = float(input("Ikkinchi sonni kiriting: "))
+# print(f"{a}+{b}=", a+b)
+# print(f"{a}-{b}=", a-b)
+# print(f"{a}x{b}=", a*b)
+# print(f"{a}/{b}=", a/b)
+
+#Mustaqil bajarilgan mashqlar
+
+# ism = input("Ismingizni kiriting : ")
+# yosh = int(input("Yoshingizni kiriting : "))
+# xabar = ism.title() + ' ' + str(yosh) + " yoshda"
+# print(xabar)
+ 
+
