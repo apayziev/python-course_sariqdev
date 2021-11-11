75e974ff7e2075b3f56c2b9f444ba5448d1a13f0
Abdulkhay Payziev <84310155+apayziev@users.noreply.github.com>
2021-11-11 16:49:11 +0500

    Solutions for the tasks

---
+++ 21-dars.Amaliyot/21-dars.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+21-DARS . FUNKSYA VA RO’YXAT 
+"""
+"""
+AMALIYOT
+"""
+
+#1]
+# matn_list = ['assalomu alekum dasturimizga xush kelibsiz', 'siz bizning birinchi mijozimizga aylandizniz', 
+#              'tashrifingizdan boshimiz osmonga yetdi', 'xayr, salomat bo\'ling']
+# def kattaHarf(matnlar):
+#     for i in range(len(matnlar)):
+#         matnlar[i] = matnlar[i].title()
+# kattaHarf(matn_list)
+# print(matn_list)
+
+#2]
+
+# matn_list = ['assalomu alekum dasturimizga xush kelibsiz', 'siz bizning birinchi mijozimizga aylandizniz', 
+#              'tashrifingizdan boshimiz osmonga yetdi', 'xayr, salomat bo\'ling']
+# def kattaHarfYangiMatn(matnchalar):
+#   matnchalar = matn_list[:]
+#   for i in range(len(matnchalar)):
+#       matnchalar[i] = matnchalar[i].title()
+#   return matnchalar 
+# result = kattaHarfYangiMatn(matn_list)
+# print(matn_list)
+# print(result)
+
+#3]
+
+# studentName = ['mamatqul', 'eshmat', 'tesha', 'mamarayim', 'boltavay']
+# def studentlarniBaholash(names):
+#     names = studentName[:]
+#     studentlar_baholari = {}
+#     for name in names:
+#         mark = int(input(f"{name.title()}ning bahosini kiriting : "))
+#         studentlar_baholari[name.title()] = mark
+#     return studentlar_baholari
+# print(studentlarniBaholash(studentName))
+# print(studentName)
+
+
+
+
---
+++ 22-dars.Amaliyot/22-dars.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+22-DARS. MOSLASHUVCHAN FUNKSIYA (*args, **kwargs)
+"""
+
+"""AMALOYOT"""
+
+#1]
+
+# def sonlarKopaytmasi(*sonlar):
+#     kopaytma = 1
+#     for son in sonlar:
+#         kopaytma*=son
+#     return kopaytma
+# print(sonlarKopaytmasi(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+#2]
+
+# def talablarHaqida(ism, familiya, **otherInfo):
+#     otherInfo['Ism'] = ism
+#     otherInfo['Familiya'] = familiya
+#     return otherInfo
+# info = talablarHaqida('Mamarayim', 'Mamatqulov', t_yil = 2000, t_joy = 'Toshkent', 
+#                       yoshi = 21, ishjoyi = 'Hozirda talaba' )
+# print(info)
